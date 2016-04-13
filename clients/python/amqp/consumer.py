65a086b32f37926e3d9416d3f71e69f49d5a3671
Pedro Costa <pmdcosta@outlook.com>
2016-04-13 04:05:33 +0100

    Config clients

--- clients/python/amqp/consumer.py
+++ clients/python/amqp/consumer.py
@@ -2,17 +2,17 @@ import pika
 from pika.credentials import ExternalCredentials
 import time
 
-server = "rabbitmq_1"
+server = "rabbitmq.qold.co"
 port = 5671
-vhost = "development"
+vhost = "dev"
 
-exchangeName = "ingestion"
+exchangeName = "gateway"
 queueName = "consumer_01"
 topic = "reports"
 
-ca_certs = '/home/pmdcosta/DockerProjects/rabbitmq/ssl/ca/cacert.pem'
-certfile = '/home/pmdcosta/DockerProjects/rabbitmq/ssl/clients/pmdcosta/cert.pem'
-keyfile = '/home/pmdcosta/DockerProjects/rabbitmq/ssl/clients/pmdcosta/key.pem'
+ca_certs = '/home/pmdcosta/Desktop/ssl/ca/ca.pem'
+certfile = '/home/pmdcosta/Desktop/ssl/pmdcosta/client.pem'
+keyfile = '/home/pmdcosta/Desktop/ssl/pmdcosta/client-key.pem'
 
 
 def onMessage(channel, method, properties, body):
@@ -21,29 +21,30 @@ def onMessage(channel, method, properties, body):
 
 
 while True:
-	try:
-		#connect
-		ssl_option = {'certfile': certfile, 'keyfile': keyfile}
-		connection = pika.BlockingConnection(
-			pika.ConnectionParameters(host=server, port=port, virtual_host=vhost, credentials=ExternalCredentials(), ssl=True, ssl_options=ssl_option, heartbeat_interval = 60))
-		channel = connection.channel()
+    try:
+        # connect
+        ssl_option = {'certfile': certfile, 'keyfile': keyfile}
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=server, port=port, virtual_host=vhost, credentials=ExternalCredentials(),
+                                      ssl=True, ssl_options=ssl_option, heartbeat_interval=60))
+        channel = connection.channel()
 
-		#declare exchange and queue, bind them and consume messages
-		channel.exchange_declare(exchange=exchangeName, exchange_type="topic", auto_delete=False)
-		channel.queue_declare(queue=queueName, exclusive=True, auto_delete=False)
-		channel.queue_bind(exchange=exchangeName, queue=queueName, routing_key=topic)
-		channel.basic_consume(consumer_callback=onMessage, queue=queueName)
-		channel.start_consuming()
-	except KeyboardInterrupt:
-		print("Shutting Down...")
-		channel.close()
-		connection.close()
-		break
+        # declare exchange and queue, bind them and consume messages
+        channel.exchange_declare(exchange=exchangeName, exchange_type="topic", auto_delete=False)
+        channel.queue_declare(queue=queueName, exclusive=True, auto_delete=False)
+        channel.queue_bind(exchange=exchangeName, queue=queueName, routing_key=topic)
+        channel.basic_consume(consumer_callback=onMessage, queue=queueName)
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print("Shutting Down...")
+        channel.close()
+        connection.close()
+        break
 
-	except Exception as e:
-		print ("Reconnecting...\nException:\n%s" % e)
-		try:
-			connection.close()
-		except:
-			pass
-		time.sleep(5)
+    except Exception as e:
+        print ("Reconnecting...\nException: " + str(e))
+        try:
+            connection.close()
+        except:
+            pass
+        time.sleep(5)
--- clients/python/mqtt/producer.py
+++ clients/python/mqtt/producer.py
@@ -1,16 +1,16 @@
 import time
 import paho.mqtt.client as mqtt
 
-server = "rabbitmq_1"
+server = "rabbitmq.qold.co"
 port = 8883
-vhost = "development"
+vhost = "dev"
 topic = "reports"
 
-client_id = "pmdcosta"
+client_id = "King"
 
-ca_certs = '/home/pmdcosta/DockerProjects/rabbitmq/ssl/ca/cacert.pem'
-certfile = '/home/pmdcosta/DockerProjects/rabbitmq/ssl/clients/pmdcosta/cert.pem'
-keyfile = '/home/pmdcosta/DockerProjects/rabbitmq/ssl/clients/pmdcosta/key.pem'
+ca_certs = '/home/pmdcosta/Desktop/ssl/ca/ca.pem'
+certfile = '/home/pmdcosta/Desktop/ssl/king/client.pem'
+keyfile = '/home/pmdcosta/Desktop/ssl/king/client-key.pem'
 
 
 def on_connect(client, userdata, flags, rc):
@@ -19,7 +19,7 @@ def on_connect(client, userdata, flags, rc):
 
 try:
     client = mqtt.Client(client_id=client_id, clean_session=True, userdata=None, protocol="MQTTv31")
-    client.tls_set(ca_certs=ca_certs, certfile=certfile, keyfile=keyfile)
+    client.tls_set(ca_certs=ca_certs, certfile=certfile, keyfile=keyfile, tls_version=2)
 
     will_payload = 'Client ' + client_id + ' disconnected!'
     client.will_set(topic, payload=will_payload, qos=1, retain=False)
@@ -32,11 +32,10 @@ try:
     msgNum = int(input("Quantity of test messages: "))
 
     for i in range(msgNum):
-        message = input('Message: ')
-        (result, mid) = client.publish(topic, payload=message, qos=1, retain=False)
-        print(result)
+        message = "Message: " + str(i)
+        (result, mid) = client.publish(topic, payload=str(message), qos=1, retain=False)
         print(mid)
-        time.sleep(1)
+        time.sleep(0.1)
 
     client.loop_stop()
     client.disconnect()
