210a407d2a01e3a3632aa53a4be2a54cbc2c5a9a
pmdcosta <pmdcosta@outlook.com>
2016-04-22 00:26:15 +0100

    Updated permissions

--- config/config-rabbit.sh
+++ config/config-rabbit.sh
@@ -6,11 +6,11 @@ if [ -z "$DEVELOPMENT" ]; then
         /opt/rabbitmq/sbin/rabbitmqctl add_vhost production
         /opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production qold ".*" ".*" ".*"
         /opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production cluster ".*" ".*" ".*"
-        /opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production gateway "^(gateway|reports|disconnects|mqtt*)$" "^(gateway|reports|disconnects|mqtt*)$" ""
+        /opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production gateway "^(gateway|reports|disconnects|mqtt.*)$" "^(gateway|reports|disconnects|mqtt.*)$" "^(mqtt.*)$"
 else
 
 	/opt/rabbitmq/sbin/rabbitmqctl add_vhost development
 	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development qold ".*" ".*" ".*"
 	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development cluster ".*" ".*" ".*"
-	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development gateway "^(gateway|reports|disconnects|mqtt*)$" "^(gateway|reports|disconnects|mqtt*)$" ""
+	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development gateway "^(gateway|reports|disconnects|mqtt.*)$" "^(gateway|reports|disconnects|mqtt.*)$" "^(mqtt.*)$"
 fi
