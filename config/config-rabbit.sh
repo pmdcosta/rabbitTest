43f7f26b1af96ce444df93be5083de5af973e6d5
pmdcosta <pmdcosta@outlook.com>
2016-05-04 23:25:02 +0100

    Refactor

--- config/config-rabbit.sh
+++ config/config-rabbit.sh
@@ -3,12 +3,11 @@ echo "========================== Set users and permissions ==================="
 /opt/rabbitmq/sbin/rabbitmqctl add_user gateway ""
 
 if [ -z "$DEVELOPMENT" ]; then
-        /opt/rabbitmq/sbin/rabbitmqctl add_vhost production
-        /opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production qold ".*" ".*" ".*"
-        /opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production cluster ".*" ".*" ".*"
-        /opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production gateway "^(gateway|reports|disconnects|mqtt.*)$" "^(gateway|reports|disconnects|mqtt.*)$" "^(mqtt.*)$"
+    /opt/rabbitmq/sbin/rabbitmqctl add_vhost production
+	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production qold ".*" ".*" ".*"
+	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production cluster ".*" ".*" ".*"
+	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production gateway "^(gateway|reports|disconnects|mqtt.*)$" "^(gateway|reports|disconnects|mqtt.*)$" "^(mqtt.*)$"
 else
-
 	/opt/rabbitmq/sbin/rabbitmqctl add_vhost development
 	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development qold ".*" ".*" ".*"
 	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development cluster ".*" ".*" ".*"
