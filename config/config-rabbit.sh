528e0e73dd713d90d92e093fc800616617ec6fc6
pmdcosta <pmdcosta@outlook.com>
2016-05-05 00:41:23 +0100

    Changed VHOST to qold

--- config/config-rabbit.sh
+++ config/config-rabbit.sh
@@ -2,14 +2,7 @@ echo "========================== Set users and permissions ==================="
 /opt/rabbitmq/sbin/rabbitmqctl add_user cluster ""
 /opt/rabbitmq/sbin/rabbitmqctl add_user gateway ""
 
-if [ -z "$DEVELOPMENT" ]; then
-    /opt/rabbitmq/sbin/rabbitmqctl add_vhost production
-	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production qold ".*" ".*" ".*"
-	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production cluster ".*" ".*" ".*"
-	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p production gateway "^(gateway|reports|disconnects|mqtt.*)$" "^(gateway|reports|disconnects|mqtt.*)$" "^(mqtt.*)$"
-else
-	/opt/rabbitmq/sbin/rabbitmqctl add_vhost development
-	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development qold ".*" ".*" ".*"
-	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development cluster ".*" ".*" ".*"
-	/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p development gateway "^(gateway|reports|disconnects|mqtt.*)$" "^(gateway|reports|disconnects|mqtt.*)$" "^(mqtt.*)$"
-fi
+/opt/rabbitmq/sbin/rabbitmqctl add_vhost qold
+/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p qold qold ".*" ".*" ".*"
+/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p qold cluster ".*" ".*" ".*"
+/opt/rabbitmq/sbin/rabbitmqctl set_permissions -p qold gateway "^(gateway|reports|disconnects|mqtt.*)$" "^(gateway|reports|disconnects|mqtt.*)$" "^(mqtt.*)$"
